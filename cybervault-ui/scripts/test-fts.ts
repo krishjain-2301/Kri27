e09fa1b876600eb4b90eba86a9613351635c90df
krishjain-2301 <jainkrish232006@gmail.com>
2026-07-09 12:02:14 +0530

    chore: remove obsolete backend SQLite scripts causing Vercel build failures

--- cybervault-ui/scripts/test-fts.ts
+++
@@ -1,8 +0,0 @@
-import { searchVault } from '../src/actions/search';
-
-async function test() {
-  console.log("Testing searchVault('Spooky')...");
-  const res = await searchVault('Spooky');
-  console.log(res);
-}
-test();
